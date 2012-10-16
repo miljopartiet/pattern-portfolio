1a517c8462c38b9ec4b2a2a91a6899e77130ea79
Joel Junström <joel.junstrom@oktavilla.se>
2012-10-16 11:43:44 +0200

    Comment links on conversations, trigger disqus on load if included in location hash

--- source/javascripts/disqus.js
+++ source/javascripts/disqus.js
@@ -3,8 +3,9 @@
 
   $(document).ready(function() {
     var element = (document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0])
+        $disqus_link = $('div.fetch-more a[href="#disqus_thread"]');
 
-    $('a[href="#disqus_thread"]').bind('click', function(e) {
+    $disqus_link.bind('click', function(e) {
       e.preventDefault();
       var $fetch_more = $(this).parent('.fetch-more');
 
@@ -15,6 +16,10 @@
       dsq.src = 'http://' + disqus_shortname + '.disqus.com/embed.js';
       element.appendChild(dsq);
     });
+
+    if (window.location.hash == '#disqus_thread') {
+      $disqus_link.trigger('click');
+    }
 
 
     var s = document.createElement('script'); s.async = true;
