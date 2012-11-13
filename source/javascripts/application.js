217f64fa7fe3d055a5b8165dcea70f8a5a10da14
Joel Junström <joel.junstrom@oktavilla.se>
2012-11-13 15:41:06 +0100

    Search as you type against remote sources

--- source/javascripts/application.js
+++ source/javascripts/application.js
@@ -18,266 +18,6 @@
     }
   };
 
-  Mp.searchAsYouType = (function() {
-    var Suggest = function(element, options) {
-      var self = this;
-      this.element = $(element);
-      if (this.element.size() == 0) {
-        return;
-      }
-
-      if (! 'suggestion_instances' in Mp) {
-        Mp.suggestion_instances = -1;
-      };
-      Mp.suggestion_instances += 1;
-      this._id = Mp.suggestion_instances;
-
-      this.focused = -1;
-      this.current_suggestions = [];
-
-      this.options = $.extend({
-        limit: false,
-        source: [],
-        onShow: $.noop,
-        onHide: $.noop,
-        onNoResults: self.hide,
-        onSetup: $.noop,
-        itemTemplate: function(item) {
-          return '<li><a href="'+ item.href +'">'+ item.name +'</a></li>';
-        },
-        sortFunction: this.defaultSortFunction,
-        inlined: false
-      }, options || {});
-
-      this.setup();
-    }
-
-    Suggest.prototype.defaultSortFunction = function(one, other) {
-      return one.name.search(this.query()) > other.name.search(this.query()) ? 1 : -1;
-    };
-
-    Suggest.prototype.setup = function() {
-      if (typeof this.options.source === 'function') {
-        var self = this;
-        setTimeout(function() {
-          self.options.source.call(self);
-        }, 20);
-      } else {
-        this.source = this.options.source;
-      }
-
-      this.container = $('<div id="suggestions-'+ this._id +'" class="suggestions" style="display:none;position:absolute;"></div>');
-
-      if (!this.options.inlined) {
-        this.container.css({
-          width: this.element.outerWidth() + 'px'
-        });
-
-        var $body = $('body');
-        this.offsets = {
-          x: parseInt($body.css('paddingLeft'), 10),
-          y: parseInt($body.css('paddingTop'), 10) + this.element.outerHeight()
-        };
-        $body.append(this.container);
-      } else {
-        this.offsets = { x: 0, y: 0 };
-        this.element.parent('form').append(this.container);
-      }
-
-      this.setupListeners();
-      this.options.onSetup.call(this);
-    };
-
-    Suggest.prototype.setupListeners = function() {
-      var self = this,
-          namespaced = function(event) {
-            return event +'.autocomplete'+ self._id;
-          };
-      this.element.bind(namespaced('keydown'), function(e) {
-        var func = self.movementKeys[e.keyCode.toString()];
-
-        if (typeof func === 'function') {
-          func.call(self, e);
-        }
-      });
-
-      this.element.bind(namespaced('keyup'), function(e) {
-        if (typeof self.movementKeys[e.keyCode.toString()] !== 'undefined') {
-          e.preventDefault();
-          return;
-        }
-        self._query = null;
-        self.search(self.query());
-      });
-
-      this.element.bind(namespaced('focus'), function() {
-        if (self.query() !== '') {
-          self.search(self.query());
-        }
-      });
-
-      this.element.bind(namespaced('blur'), function() {
-        self.focusSuggestion(-1);
-      });
-
-      if (!this.options.inlined) {
-        $(window).bind(namespaced('resize'), function() {
-          self.position();
-        });
-      }
-    };
-
-    Suggest.prototype.query = function() {
-      if (!this._query) {
-        this._query = $.trim(this.element.val());
-      }
-
-      return this._query;
-    };
-
-    Suggest.prototype.movementKeys = {
-      '16': null, // Shift
-      '27': function() { // esc
-        this.hide();
-        this.element.blur();
-      },
-      '37': null, // Arrow left
-      '38': function(e) { // Arrow up
-        this.focusPrevious();
-        e.preventDefault(e);
-      },
-      '39': null, // Arrow right
-      '40': function(e) { // Arrow down
-        this.focusNext();
-        e.preventDefault();
-      },
-      '9': function(e) { // tab
-        !e.shiftKey ? this.focusNext() : this.focusPrevious();
-        e.preventDefault();
-      },
-      '13': function(e) { // enter
-        e.preventDefault();
-        this.container.find("a.focused").trigger("click");
-      }
-    };
-
-    Suggest.prototype.search = function(value) {
-      var value = this.query();
-      if (value === '') {
-        this.update([]);
-        this.hide();
-      } else {
-        var matches = this.searchSources(value);
-        if (matches.length !== 0) {
-          if (this.options.limit !== false) {
-            matches = matches.splice(0, this.options.limit);
-          }
-          this.update(matches);
-          if (this.container.is(':hidden')) {
-            this.show();
-          }
-        } else {
-          this.options.onNoResults.call(this);
-        }
-      }
-    };
-
-    Suggest.prototype.searchSources = function(value) {
-      var self = this,
-          pattern = [],
-          values, test;
-
-      values = value.replace(/\s+/, ' ').split(' ');
-      if (values.length > 1) {
-        for (var i = 0, j = values.length; i < j; i++) {
-          pattern.push('(?=.*'+values[i]+')');
-        }
-      } else {
-        pattern.push(value);
-      }
-
-      test = new RegExp(pattern.join(''), 'gi');
-
-      return $.grep(this.source, function(match) {
-        return test.exec(match.name) !== null;
-      }).sort(function(one, other) {
-        return self.options.sortFunction.call(self, one, other);
-      });
-    };
-
-    Suggest.prototype.update = function(matches) {
-      this.current_suggestions = matches;
-      this.focused = -1;
-
-      var self = this,
-          html = ['<ul>'];
-      $.each(this.current_suggestions, function(i, item) {
-        html.push(self.options.itemTemplate.call(self, item, self.query()));
-      });
-      html.push('</ul>');
-
-      this.container.html(html.join(''));
-    };
-
-    Suggest.prototype.show = function() {
-      this.position();
-      this.container.show();
-      var self = this;
-      $(document).bind(('click.autocomplete'+ this._id), function(e) {
-        var el = self.container.get(0);
-        if (e.target !== self.element.get(0) && e.target !== el && !$.contains(el, e.target)) {
-          self.hide();
-          self.element.blur();
-        }
-      });
-      this.options.onShow.call(this);
-    };
-
-    Suggest.prototype.hide = function() {
-      this.container.hide();
-      $(document).unbind('click.autocomplete-'+ this._id);
-      this.options.onHide.call(this);
-    };
-
-    Suggest.prototype.focusNext = function() {
-      var num = this.current_suggestions.length;
-      if (num > 0 && this.focused < (num - 1)) {
-        this.focusSuggestion(this.focused + 1);
-      } else if (this.focused === (num - 1)) {
-        this.focusSuggestion(0);
-      }
-    };
-
-    Suggest.prototype.focusPrevious = function() {
-      if (this.focused !== -1) {
-        this.focusSuggestion(this.focused - 1);
-      } else {
-        this.focusSuggestion(this.current_suggestions.length - 1);
-      }
-    };
-
-    Suggest.prototype.focusSuggestion = function(num) {
-      this.container.find('li').removeClass('focused')
-        .filter(':nth-child('+ (num + 1) +')')
-        .addClass('focused');
-      this.focused = num;
-    };
-
-    Suggest.prototype.position = function() {
-      if (this.options.inlined) {
-        return;
-      }
-      var position = this.element.offset();
-      this.container.css({
-        'top': Math.round(position.top + this.offsets.y) + 'px',
-        'left': Math.round(position.left + this.offsets.x) + 'px'
-      });
-    };
-
-    return function(selector, options) {
-      new Suggest(selector, options);
-    };
-  }());
 
   Mp.NavigationToggler = function(element) {
     var $toggler = $(element),
@@ -516,6 +256,51 @@
         this.element.parent().removeClass('active-suggestions');
       }
     });
+
+    Mp.searchAsYouType('#main-search', {
+      limit: 25,
+      remote: true,
+      inlined: true,
+      itemTemplate: function(item, query) {
+        var html = ['<li><a href="'+item.href+'">'],
+        name = item.name,
+        matches = [],
+        words = query.split(' ');
+
+        for (var i = 0, j = words.length; i < j; i++) {
+          var word_matches = name.match(new RegExp(words[i], 'gi'));
+          if (word_matches) {
+            matches = matches.concat(word_matches);
+          }
+        }
+
+        if (matches.length > 0) {
+          for (var i = 0, j = matches.length; i < j; i++) {
+            name = name.replace(matches[i], '<em>'+ matches[i] +'</em>');
+          }
+        }
+
+        html.push('<span class="name">');
+        html.push(name);
+        html.push('</span>');
+        html.push('<span class="category">');
+        html.push(item.category);
+        html.push('</span>');
+        html.push('</a></li>');
+
+        return html.join('');
+      },
+      onSetup: function() {
+        this.container.addClass('main-search');
+      },
+      onShow: function() {
+        this.element.addClass('has-suggestions');
+      },
+      onHide: function() {
+        this.element.removeClass('has-suggestions');
+      }
+    });
+
 
     $('section.tabbed').tabs();
     $('#conversation-form').hide();
