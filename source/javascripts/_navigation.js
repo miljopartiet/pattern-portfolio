9aac2bf60b9f2b453927840bd79b56d87a12c786
Joel Junström <joel.junstrom@oktavilla.se>
2014-03-05 13:11:53 +0100

    Detach header on scroll

--- source/javascripts/_navigation.js
+++ source/javascripts/_navigation.js
@@ -2,6 +2,48 @@
   "use strict";
   namespace("Mp");
 
+  Mp.Navigation = (function() {
+    function Navigation() {
+      this.header = $("#site-banner");
+    }
+
+    $.extend(Navigation.prototype, {
+      attach: function() {
+        if (this._detached) {
+          this.header.removeClass("detached");
+          this._detached = false;
+        }
+      },
+
+      detach: function() {
+        if (!this._detached) {
+          this.header.addClass("detached");
+          this._detached = true;
+        }
+      },
+
+      visible: function() {
+        if (!this._visible) {
+          this.header.removeClass("hidden");
+          this.header.addClass("visible");
+        }
+
+        this._visible = true;
+      },
+
+      hidden: function() {
+        if (this._visible) {
+          this.header.removeClass("visible");
+          this.header.addClass("hidden");
+        }
+
+        this._visible = false;
+      }
+    });
+
+    return Navigation;
+  }());
+
   Mp.NavigationToggler = function(toggler, target) {
     var toggle = function(event) {
       event.preventDefault();
@@ -13,4 +55,53 @@
 
     $(toggler).bind("click", toggle);
   };
+
+  function detachNavigationOnScroll(navigation) {
+    var MIN_OFFSET = 80,
+        lastValue = 0;
+
+    var scrollDirection = function(newValue, lastValue) {
+      var direction = {};
+
+      if (newValue < lastValue) {
+        direction.direction = "up";
+        direction.offset = lastValue - newValue;
+      } else {
+        direction.direction = "down";
+        direction.offset = newValue - lastValue;
+      }
+
+      return direction;
+    };
+
+    $(window).scroll(_.debounce(function() {
+      var newValue = $(document).scrollTop(),
+          direction = scrollDirection(newValue, lastValue);
+
+
+      if (direction.offset > MIN_OFFSET) {
+        lastValue = newValue;
+      } else {
+        return;
+      }
+
+      if (newValue <= 0) {
+        navigation.attach();
+      } else {
+        navigation.detach();
+      }
+
+      if (direction.direction == "up") {
+        navigation.visible();
+      } else {
+        navigation.hidden();
+      }
+
+    }), 150);
+  }
+
+  $(function() {
+    var navigation = new Mp.Navigation();
+    detachNavigationOnScroll(navigation);
+  });
 }(jQuery));
